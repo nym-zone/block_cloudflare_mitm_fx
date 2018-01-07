c50bfa5f85df730a977813ac5bd4dc0007a1fe5d
nullius <nullius@nym.zone>
2018-01-07 04:42:39 +0000

    Pull v1.0.10.1 from a.m.o.; change to MIT license
     - Pull v1.0.10.1 from a.m.o.  Source files here included as-is.
    
     - Change to MIT license.  Closes #6.

--- src/setwhitelist.js
+++ src/setwhitelist.js
@@ -17,10 +17,12 @@ browser.storage.local.set({myset_cfwhite: cf_tmpdata});document.querySelector("#
 //workaround - simplewarn didn't work as expected if ign3p is active
 if (document.querySelector("#myset_xsimplewarn_1").checked){document.querySelector("#myset_xign3p").checked=false;}
 //ADVANCED
+if (document.querySelector("#myset_xautoclean").checked){browser.storage.local.set({myset_xautoclean: "y"});}else{browser.storage.local.set({myset_xautoclean: "n"});}
 if (document.querySelector("#myset_xincapsula").checked){browser.storage.local.set({myset_xincapsula: "y"});}else{browser.storage.local.set({myset_xincapsula: "n"});}
 if (document.querySelector("#myset_xgshield").checked){browser.storage.local.set({myset_xgshield: "y"});}else{browser.storage.local.set({myset_xgshield: "n"});}
 if (document.querySelector("#myset_xsucuri").checked){browser.storage.local.set({myset_xsucuri: "y"});}else{browser.storage.local.set({myset_xsucuri: "n"});}
 if (document.querySelector("#myset_xign3p").checked){browser.storage.local.set({myset_xign3p: "y"});}else{browser.storage.local.set({myset_xign3p: "n"});}
+if (document.querySelector("#myset_xwhitemark").checked){browser.storage.local.set({myset_xwhitemark: "y"});}else{browser.storage.local.set({myset_xwhitemark: "n"});}
 //ACTION
 if (document.querySelector("#myset_xsimplewarn_0").checked){browser.storage.local.set({myset_xsimplewarn:0});}
 if (document.querySelector("#myset_xsimplewarn_1").checked){browser.storage.local.set({myset_xsimplewarn:1});}
@@ -32,18 +34,19 @@ function setCurrentChoice(r){
 //WHITELIST
 document.querySelector("#myset_cfwhite").value = r.myset_cfwhite||"";
 //ADVANCED
+if (r.myset_xautoclean=='y'){document.querySelector("#myset_xautoclean").checked=true;}else{document.querySelector("#myset_xautoclean").checked=false;}
 if (r.myset_xincapsula=='y'){document.querySelector("#myset_xincapsula").checked=true;}else{document.querySelector("#myset_xincapsula").checked=false;}
 if (r.myset_xgshield=='y'){document.querySelector("#myset_xgshield").checked=true;}else{document.querySelector("#myset_xgshield").checked=false;}
 if (r.myset_xsucuri=='y'){document.querySelector("#myset_xsucuri").checked=true;}else{document.querySelector("#myset_xsucuri").checked=false;}
 if (r.myset_xign3p=='y'){document.querySelector("#myset_xign3p").checked=true;}else{document.querySelector("#myset_xign3p").checked=false;}
+if (r.myset_xwhitemark=='y'){document.querySelector("#myset_xwhitemark").checked=true;}else{document.querySelector("#myset_xwhitemark").checked=false;}
 if (r.myset_xsimplewarn){switch(r.myset_xsimplewarn){
 case 1:document.querySelector("#myset_xsimplewarn_1").checked=true;break;
 case 2:document.querySelector("#myset_xsimplewarn_2").checked=true;break;
 default:document.querySelector("#myset_xsimplewarn_0").checked=true;break;
 }}else{document.querySelector("#myset_xsimplewarn_0").checked=true;}
 }
-var getting=browser.storage.local.get();
-getting.then(setCurrentChoice, onError);
+var getting=browser.storage.local.get();getting.then(setCurrentChoice,onError);
 }
 document.addEventListener("DOMContentLoaded", loadWhitelist);
 document.querySelector("form").addEventListener("submit", saveWhitelist);
